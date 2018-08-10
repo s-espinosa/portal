d989af806fcbeec0232138b238d04fecd92fc763
Sal Espinosa <sespinos@gmail.com>
2018-08-10 13:00:27 -0600

    Move cohort to its own table

---
+++ db/migrate/20180810180937_add_cohort_to_users.rb
@@ -0,0 +1,5 @@
+class AddCohortToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :users, :cohort, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_195843) do
+ActiveRecord::Schema.define(version: 2018_08_10_181026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "name"
+    t.string "census_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -32,7 +39,6 @@ ActiveRecord::Schema.define(version: 2018_08_06_195843) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "cohort_name"
     t.string "email"
     t.string "first_name"
     t.string "last_name"
@@ -44,8 +50,11 @@ ActiveRecord::Schema.define(version: 2018_08_06_195843) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "cohort_id"
+    t.index ["cohort_id"], name: "index_users_on_cohort_id"
   end
 
   add_foreign_key "rubrics", "projects"
   add_foreign_key "rubrics", "rubric_categories"
+  add_foreign_key "users", "cohorts"
 end
