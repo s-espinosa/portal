57dd9297792d4c07cb195844a4562739b9d032d0
Sal Espinosa <sespinos@gmail.com>
2018-09-12 13:56:23 -0600

    Update specs to refer to staging values

--- spec/poros/census_flock_spec.rb
+++ spec/poros/census_flock_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'CensusFlock' do
   it 'can create a group of students given a cohort id' do
-    CensusFlock.create_or_update(46)
+    CensusFlock.create_or_update(4)
 
-    norm = User.find_by(census_id: 586)
+    norm = User.find_by(census_id: 80)
 
-    expect(norm.first_name).to eq("Norman")
+    expect(norm.first_name).to eq("Fake")
   end
 end
